907ae2475b3aa979054bd4547c9e6ae0d598b305
Calle1122 <60339644+Calle1122@users.noreply.github.com>
2021-03-12 12:50:49 +0100

    spelaren kan nu köpa items

--- Gklass/Program.cs
+++ Gklass/Program.cs
@@ -7,15 +7,28 @@ namespace Gklass
     {
         static void Main(string[] args)
         {
+            int money = 50;
+
             Dictionary<string, int> shopItems = new Dictionary<string, int>();
 
             shopItems.Add("Bronze Sword", 15);
             shopItems.Add("Chainmail Chestplate", 30);
             shopItems.Add("Shining Catalyst", 50);
 
+            Console.WriteLine("Money: " + money + "\n");
+
             foreach (string key in shopItems.Keys)
             {
                 Console.WriteLine(key + ": " + shopItems[key]);
+            }
+
+            Console.WriteLine("\nPlease enter the name of the item you would like to purchase.");
+            string playerInput = Console.ReadLine();
+
+            if(shopItems.ContainsKey(playerInput)){
+                System.Console.WriteLine("\nYou bought the " + playerInput + " for " + shopItems[playerInput] + "$");
+
+                money = money - shopItems[playerInput];
             }
 
             Console.ReadLine();
